d90ae06e8f5afaf93d8ad9e89f351b05198e0d49
Nguyen Thai Quang Ha <quangha2001cute@gmail.com>
2022-11-25 17:47:06 +0700

    category + products

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ app.post("/api/login", (req, res) => {
     }
   );
 });
+
+// all san pham
 app.get('/api/products',(req,res) =>{
   db.connect(function(err) {
     // if (err) throw err;
@@ -63,6 +65,56 @@ app.get('/api/products',(req,res) =>{
   });
 })
 
+// san pham moi nhat
+app.get('/api/newproducts',(req,res) =>{
+  db.connect(function(err) {
+    // if (err) throw err;
+    // if connection is successful
+  db.query("SELECT * FROM products ORDER BY id_product DESC LIMIT 0,6", function (err, result) {
+      // if any error while executing above query, throw error
+      if (err) throw err;
+      // if there is no error, you have the result
+      console.log(result);
+      
+      res.send(result)
+    });
+  });
+})
+
+// danh muc
+app.get('/api/category',(req,res)=>{
+db.connect(function(err) {
+  db.query("SELECT * from category", function(err,result){
+    if(err) throw err;
+    console.log(result);
+    res.send(result);
+  })
+})
+});
+
+// 
+
+// app.get('/api/pruducts/category_id/',(req,res)=>{
+  
+//   db.connect(function(err) {
+//     db.query("SELECT * from products where category_id=1", function(err,result){
+//       if(err) throw err;
+//       console.log(result);
+//       res.send(result);
+//     })
+//   })
+//   })
+
+
+// app.get('/api/news',(req,res)=>{
+//   db.connect(function(err) {
+//     db.query("SELECT * from news", function(err,result){
+//       if(err) throw err;
+//       console.log(result);
+//       res.send(result);
+//     })
+//   })
+//   })
 // app.get("/", (req, res) => {
 //   res.send("server ís running");
 // });
