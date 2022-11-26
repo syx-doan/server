2e788571c4e405b8cb0d3727446727918a8b3b6e
nguyen thanh <thanhnnpd04843@fpt.edu.vn>
2022-11-26 20:01:37 +0700

    san pham moi

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "do_an_tot_nghiep",
+// });
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// app.post("/api/insert", (req, res) => {
+//   const fullname = req.body.fullname;
+//   const password = req.body.password;
+//   const email = req.body.email;
+//   const sqlInsert =
+//     "INSERT INTO `users` ( `fullname`, `password`,`email`) VALUES (?,?,?);";
+//   db.query(sqlInsert, [fullname, password, email], (err, result) => {
+//     console.log(err);
+//   });
+// });
+// app.post("/api/login", (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   db.query(
+//     "SELECT * FROM users WHERE `email` = ? AND password = ?",
+//     [email, password],
+//     (err, result) => {
+//       if (err) {
+//         res.send({ err: err });
+//       }
+//       if (result.length > 0) {
+//         res.send(result);
+//       } else {
+//         res.send({ message: "Không có tài khoản, hoặc mất khẩu không đúng" });
+//       }
+//     }
+//   );
+// });
+
+// // all san pham
+// app.get('/api/products',(req,res) =>{
+//   db.connect(function(err) {
+//     // if (err) throw err;
+//     // if connection is successful
+//   db.query("SELECT * FROM products", function (err, result) {
+//       // if any error while executing above query, throw error
+//       if (err) throw err;
+//       // if there is no error, you have the result
+//       console.log(result);
+
+//       res.send(result)
+//     });
+//   });
+// })
+
+// // san pham moi nhat
+// app.get('/api/newproducts',(req,res) =>{
+//   db.connect(function(err) {
+//   db.query("SELECT * FROM products ORDER BY id_product DESC LIMIT 0,6", function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+      
+//       res.send(result)
+//     });
+//   });
+// })
+
+// // danh muc
+// app.get('/api/category',(req,res)=>{
+// db.connect(function(err) {
+//   db.query("SELECT * from category", function(err,result){
+//     if(err) throw err;
+//     console.log(result);
+//     res.send(result);
+//   })
+// })
+// });
+
+
+// app.listen(4000, () => {
+//   console.log("rungning in port 400");
+// });
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
@@ -13,12 +101,24 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get("/api/get", (req, res) => {
-//   const sqlSelect = "SELECT * FROM `users` ";
-//   db.query(sqlSelect, (err, result) => {
-//     res.send(result);
-//   });
-// });
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM `users` ";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+app.get("/api/brand", (req, res) => {
+  const sqlSelect = "SELECT * FROM `brand` ";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+app.get("/api/category", (req, res) => {
+  const sqlSelect = "SELECT * FROM `category` ";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
 
 app.post("/api/insert", (req, res) => {
   const fullname = req.body.fullname;
@@ -48,77 +148,57 @@ app.post("/api/login", (req, res) => {
     }
   );
 });
-
-// all san pham
-app.get('/api/products',(req,res) =>{
-  db.connect(function(err) {
-    // if (err) throw err;
-    // if connection is successful
-  db.query("SELECT * FROM products", function (err, result) {
-      // if any error while executing above query, throw error
+app.get("/api/products", (req, res) => {
+  db.connect(function (err) {
+    db.query("SELECT * FROM products", function (err, result) {
       if (err) throw err;
-      // if there is no error, you have the result
       console.log(result);
 
-      res.send(result)
+      res.send(result);
     });
   });
-})
-
-// san pham moi nhat
-app.get('/api/newproducts',(req,res) =>{
-  db.connect(function(err) {
-    // if (err) throw err;
-    // if connection is successful
-  db.query("SELECT * FROM products ORDER BY id_product DESC LIMIT 0,6", function (err, result) {
-      // if any error while executing above query, throw error
-      if (err) throw err;
-      // if there is no error, you have the result
-      console.log(result);
-      
-      res.send(result)
-    });
-  });
-})
-
-// danh muc
-app.get('/api/category',(req,res)=>{
-db.connect(function(err) {
-  db.query("SELECT * from category", function(err,result){
-    if(err) throw err;
-    console.log(result);
-    res.send(result);
-  })
-})
 });
 
-// 
+app.get("/api/newproducts", (req, res) => {
+  db.connect(function (err) {
+    db.query(
+      "SELECT * FROM products ORDER BY id_product DESC LIMIT 6",
+      function (err, result) {
+        if (err) throw err;
+        console.log(result);
 
-// app.get('/api/pruducts/category_id/',(req,res)=>{
-  
-//   db.connect(function(err) {
-//     db.query("SELECT * from products where category_id=1", function(err,result){
-//       if(err) throw err;
-//       console.log(result);
-//       res.send(result);
-//     })
-//   })
-//   })
-
-
-// app.get('/api/news',(req,res)=>{
-//   db.connect(function(err) {
-//     db.query("SELECT * from news", function(err,result){
-//       if(err) throw err;
-//       console.log(result);
-//       res.send(result);
-//     })
-//   })
-//   })
-// app.get("/", (req, res) => {
-//   res.send("server ís running");
-// });
+        res.send(result);
+      }
+    );
+  });
+});
 
 app.listen(4000, () => {
   console.log("rungning in port 400");
+});
+
+app.get("/api/categoryproduct", (req, res) => {
+  const categoryid = req.body.categoryid;
+  if (categoryid) {
+    db.connect(function (err) {
+      db.query(
+        `SELECT * FROM products WHERE category_id = ${categoryid}`,
+        function (err, result) {
+          if (err) throw err;
+          console.log(result);
+
+          res.send(result);
+        }
+      );
+    });
+  } else {
+    db.connect(function (err) {
+      db.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+        res.send(result);
+      });
+    });
+  }
 });
