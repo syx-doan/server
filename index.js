f36455a1110939408972ae66fd6c7755d173a8a9
nguyen thanh <thanhnnpd04843@fpt.edu.vn>
2022-12-09 17:22:08 +0700

    thanh

--- index.js
+++ index.js
@@ -42,35 +42,55 @@ app.post("/api/dangky", (req, res) => {
     console.log(err);
   });
 });
+// app.post("/api/thanhtoan", (req) => {
+//   const { data } = req.body;
+//   const { id_user, fullname, address, carts } = data;
+//   const sqlInsert =
+//     "INSERT INTO `bill` ( `id_user`,`fullname`,`address`) VALUES (?,?,?);";
+//   const sqlDetail =
+//     "INSERT INTO bill_dentail (total,price,id_bill,id_product) VALUES ?;";
+//   db.query(sqlInsert, [id_user, fullname, address], (err, result) => {
+//     if (result) {
+//       const dataCart = [];
+//       carts.forEach((cart) => {
+//         dataCart.push([cart.qty, cart.price, result.insertId, cart.id_product]);
+//       });
+//       db.query(sqlDetail, [dataCart], (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     }
+//   });
+// });
 app.post("/api/thanhtoan", (req) => {
   // const name = req.body.name;
   // const phone = req.body.phone;
   // const address = req.body.address;
   // const district = req.body.district;
   // const city = req.body.city;
-  
- 
-  const {data} = req.body
-  const {id_user,fullname,address,carts} =data
+
+  const { data } = req.body;
+  const { id_user, fullname, note, address, carts } = data;
   const sqlInsert =
-  "INSERT INTO `bill` ( `id_user`,`fullname`,`address`) VALUES (?,?,?);";
+    "INSERT INTO `bill` ( `id_user`,`fullname`,`address`,`note`) VALUES (?,?,?,?);";
   const sqlDetail =
-  "INSERT INTO bill_dentail (total,price,id_bill,id_product) VALUES ?;";
-  db.query(sqlInsert, [id_user,fullname,address], (err, result) => {
-    if(result){     
-      const dataCart = []
-      carts.forEach(cart => {
-        dataCart.push([cart.qty,cart.price,result.insertId,cart.id_product])
-      })
-      db.query(sqlDetail,  [dataCart] , (err, result) => {
-        if(err){
-          console.log(err)
+    "INSERT INTO bill_dentail (total,price,id_bill,id_product) VALUES ?;";
+  db.query(sqlInsert, [id_user, fullname, address, note], (err, result) => {
+    if (result) {
+      const dataCart = [];
+      carts.forEach((cart) => {
+        dataCart.push([cart.qty, cart.price, result.insertId, cart.id_product]);
+      });
+      db.query(sqlDetail, [dataCart], (err, result) => {
+        if (err) {
+          console.log(err);
         }
-     
-    })
-  }
+      });
+    }
+  });
 });
-});
+
 //đăng nhập
 app.post("/api/login", (req, res) => {
   const email = req.body.email;
@@ -94,18 +114,12 @@ app.post("/api/login", (req, res) => {
 // all san pham
 app.get("/api/products", (req, res) => {
   db.connect(function (err) {
-    // if (err) throw err;
-    // if connection is successful
-    db.query(
-      "SELECT * FROM products",
-      function (err, result) {
-        // if any error while executing above query, throw error
-        if (err) throw err;
-        console.log(result);
+    db.query("SELECT * FROM products", function (err, result) {
+      if (err) throw err;
+      console.log(result);
 
-        res.send(result);
-      }
-    );
+      res.send(result);
+    });
   });
 });
 
@@ -144,19 +158,6 @@ app.get("/api/newproducts", (req, res) => {
     );
   });
 });
-
-//
-
-// app.get('/api/pruducts/category_id/',(req,res)=>{
-
-//   db.connect(function(err) {
-//     db.query("SELECT * from products where category_id=1", function(err,result){
-//       if(err) throw err;
-//       console.log(result);
-//       res.send(result);
-//     })
-//   })
-//   })
 
 app.get("/api/news", (req, res) => {
   db.connect(function (err) {
